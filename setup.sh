ab25ac6cf105fede6e0311018fc41609f37618de
JB <jeanbastien.grill@gmail.com>
2018-07-02 16:24:50 +0200

    tt

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@ mkdir games
 for gnum in {1480..1680}
 do
 	wget https://s3.amazonaws.com/lczero/training/games${gnum}0000.tar.gz
-	mkdir games/games${gnum}0000
-	tar -xzf games${gnum}0000.tar.gz -C games${gnum}0000/
-	gzip games/games${gnum}0000/training.*
+	mkdir "games/games${gnum}0000"
+	tar -xzf "games${gnum}0000.tar.gz" -C "games${gnum}0000/"
+	gzip "games/games${gnum}0000/training.*"
+	echo "${gnum} done"
 done

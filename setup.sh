7b3cea46a43fcccc88566a8408dea5b0adeb2edd
Pytq <jeanbastien.grill@gmail.com>
2018-07-05 17:42:47 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,9 @@ for gnum in {1480..1680}
 do
 	wget https://s3.amazonaws.com/lczero/training/games${gnum}0000.tar.gz
 	mkdir "games/games${gnum}0000"
-	tar -xzf "games${gnum}0000.tar.gz" -C "games${gnum}0000/"
+	echo "Extracting games ${gnum}..."
+	tar -xzf "games${gnum}0000.tar.gz" -C "games/games${gnum}0000/"
+	echo "zipping games ${gnum}..."
 	gzip "games/games${gnum}0000/training.*"
 	echo "${gnum} done"
 done
